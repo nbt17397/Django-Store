21628e9ca41251c42ec345e326fe343793826043
Mason <tu.nguyenba@haophuong.com>
2022-08-01 10:11:41 +0700

    .

--- store/settings.py
+++ store/settings.py
@@ -173,9 +173,11 @@ USE_TZ = True
 # MEDIA_ROOT = '%s/project/static' % BASE_DIR
 
 
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'static'),
+# )
 django_heroku.settings(locals())
 
 
